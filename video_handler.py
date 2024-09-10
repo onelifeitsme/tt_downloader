6ae93ab1c7e6530593858068b20589c7968085f1
Artur Karpovich <onelifeitsme@gmail.com>
2024-09-10 16:46:12 +0300

    11

--- video_handler.py
+++ video_handler.py
@@ -10,7 +10,7 @@ logger = logging.getLogger(__name__)
 from aiohttp_socks import ProxyConnector
 
 # Создаем соединение через SOCKS5-прокси
-connector = ProxyConnector.from_url('socks5://host.docker.internal:9050')
+connector = ProxyConnector.from_url('socks5://217.171.146.18:9050')
 
 
 headers = {
@@ -74,7 +74,7 @@ async def download_video(video_url, session):
 
 
 async def get_video(url, dev=False):
-    connector = ProxyConnector.from_url('socks5://host.docker.internal:9050')
+    connector = ProxyConnector.from_url('socks5://217.171.146.18:9050')
 
     async with aiohttp.ClientSession(connector=connector) as session:
         try:
